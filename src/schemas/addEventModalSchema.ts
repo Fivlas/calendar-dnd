28a9b4ba1e4d6219d09eda3b5048cb84e41ba5d3
Fivlas <filip.skoczylas777007@gmail.com>
2025-01-02 15:26:50 +0100

    adds end validation to not to be earlier then starting date

--- src/schemas/addEventModalSchema.ts
+++ src/schemas/addEventModalSchema.ts
@@ -1,21 +1,26 @@
 import { z } from "zod";
 
-export const addEventModalSchema = z.object({
-    id: z.string().optional(),
-    startDate: z.date({
-        required_error: "Please select a date and time",
-        invalid_type_error: "That's not a date!",
-    }),
-    endDate: z.date({
-        required_error: "Please select a date and time",
-        invalid_type_error: "That's not a date!",
-    }),
-    title: z
-        .string()
-        .min(2, {
-            message: "Title must be at least 2 characters long",
-        })
-        .max(50, {
-            message: "Title must be at most 50 characters long",
+export const addEventModalSchema = z
+    .object({
+        id: z.string().optional(),
+        startDate: z.date({
+            required_error: "Please select a date and time",
+            invalid_type_error: "That's not a date!",
         }),
-});
+        endDate: z.date({
+            required_error: "Please select a date and time",
+            invalid_type_error: "That's not a date!",
+        }),
+        title: z
+            .string()
+            .min(2, {
+                message: "Title must be at least 2 characters long",
+            })
+            .max(50, {
+                message: "Title must be at most 50 characters long",
+            }),
+    })
+    .refine((data) => data.endDate >= data.startDate, {
+        message: "End date cannot be earlier than the start date",
+        path: ["endDate"],
+    });
